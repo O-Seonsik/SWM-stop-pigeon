1f96aa93f62e8ed21409e25240e169ae622d6faa
5linesys <dhtjstlr777@gmail.com>
2021-05-14 00:34:37 +0900

    Merge something

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,21 @@
+import react, { useState } from 'react';
 import Map from 'components/Map';
 import SearchPanel from 'components/search/SearchPanel';
+import FilterGroup from "../components/filter/FilterGroup";
+import ActionGroup from "../components/action/ActionGroup";
 
 const Main = () => {
-  return (
-    <div id="main">
-      <SearchPanel />
-      <Map />
-    </div>
-  );
+    const [keyword, setKeyword] = useState();
+    const [searchResult, setSearchResult] = useState();
+    console.log(keyword);
+    return (
+        <div id="main">
+            <SearchPanel setKeyword={setKeyword} searchResult={searchResult} />
+            <Map keyword={keyword} setSearchResult={setSearchResult}/>
+            <FilterGroup />
+            <ActionGroup />
+        </div>
+    );
 }
 
 export default Main;
