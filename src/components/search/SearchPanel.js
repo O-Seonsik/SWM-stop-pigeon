ca2b8f3fe708fcbb4d047bf44a9bea51a7d93cf5
bluewood <bluewood.truth@gmail.com>
2021-05-13 17:52:26 +0900

    feat: add search footer

--- src/components/search/SearchPanel.js
+++ src/components/search/SearchPanel.js
@@ -1,10 +1,12 @@
 import Header from './header/Header';
+import Footer from './footer/Footer';
 import './SearchPanel.css';
 
 function SearchPanel() {
   return (
     <div className='search-panel'>
       <Header />
+      <Footer />
     </div>
   )
 }
