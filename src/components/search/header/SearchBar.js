3958b5d1c7f9f1963a7d2a5c4ce12f5854085beb
bluewood <bluewood.truth@gmail.com>
2021-05-14 03:19:25 +0900

    feat: add Enter key search

--- src/components/search/header/SearchBar.js
+++ src/components/search/header/SearchBar.js
@@ -2,16 +2,29 @@ import React, { useState } from 'react';
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
-    const search = () => {
-        let input = document.getElementById('input-search');
-        props.setKeyword(input.value)
+  const search = () => {
+      let input = document.getElementById('input-search');
+      props.setKeyword(input.value)
+  }
+  
+  const onKeyPress = (e) => {
+    if (e.key == "Enter") {
+      search();
     }
+  }
 
   return (
     <div className="search-bar-wrap">
       <div className="input-box">
-        <input type="text" id="input-search" placeholder="장소, 주소 검색"/>
-        <button className="search-icon" onClick={search}>검색</button>
+        <input
+          type="text"
+          id="input-search"
+          placeholder="장소, 주소 검색"
+          onKeyPress={onKeyPress}
+        />
+        <button className="search-icon" onClick={search}>
+          검색
+        </button>
       </div>
     </div>
   );
