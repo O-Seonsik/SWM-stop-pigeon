99822c6c78b463647421a66a97cb24ae2aad346d
bluewood <bluewood.truth@gmail.com>
2021-05-13 23:02:31 +0900

    feat: merge map and search

---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import Map from 'components/Map';
+import SearchPanel from 'components/search/SearchPanel';
+
+const Main = () => {
+  return (
+    <div id="main">
+      <SearchPanel />
+      <Map />
+    </div>
+  );
+}
+
+export default Main;
