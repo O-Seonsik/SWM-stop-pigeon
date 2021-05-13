b358daab7c894a22dc22dafa3392537fe42665a3
5linesys <dhtjstlr777@gmail.com>
2021-05-14 08:36:27 +0900

    Finish marker

--- src/components/filter/FilterGroup.js
+++ src/components/filter/FilterGroup.js
@@ -1,15 +1,23 @@
 import './FilterGroup.css'
 import FilterItem from './FilterItem.js'
 
-function toggleMarker(isOn) {
-  console.log(`toggle marker: ${isOn}`);
-}
 
-function toggleGrid(isOn) {
-  console.log(`toggle grid: ${isOn}`);
-}
+const FilterGroup = (props) => {
+    const toggleMarker = (isOn) => {
+      if(isOn){
+          props.showMarkers();
+      }else{
+          props.hideMarkers();
+      }
+    }
 
-function FilterGroup() {
+    const toggleGrid = (isOn) => {
+        if(isOn){
+            props.showGrids();
+        }else{
+            props.hideGrids();
+        }
+    }
   return (
     <div id="filter-group">
       <FilterItem
@@ -17,12 +25,14 @@ function FilterGroup() {
         label="비둘기 분포 마커 표시"
         defaultChecked={true}
         onChangeEvent={toggleMarker}
+        setIsMarker={props.setIsMarker}
       />
       <FilterItem
         id="grid-check"
         label="비둘기 분포 그리드 표시"
         defaultChecked={false}
         onChangeEvent={toggleGrid}
+        setIsGrid={props.setIsGrid}
       />
     </div>
   );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -20,6 +20,8 @@ const Main = () => {
     const [keyword, setKeyword] = useState();
     const [searchResult, setSearchResult] = useState();
     const [isSearch, setIsSearch] = useState();
+    const [isGrid, setIsGrid] = useState(false);
+    const [isMarker, setIsMarker] = useState(true);
 
     let map;
     useEffect(() => {
@@ -48,9 +50,14 @@ const Main = () => {
             var center_position = map.getCenter();
             squareTestLevel(level, center_position['Ma'], center_position['La']);
         });
+        markerTest();
     }, []);
 
-
+    const test = () => {
+        var level = map.getLevel();
+        var center_position = map.getCenter();
+        squareTestLevel(level, center_position['Ma'], center_position['La']);
+    }
 
     // 검색
     // 장소 검색 객체를 생성합니다
@@ -181,6 +188,7 @@ const Main = () => {
 
     // 마커 테스트 코드
     const markerTest = () => {
+        console.log("!!!!!");
         let markerData = JSON.stringify(MarkerPosition);
         markerData = JSON.parse(markerData)['data'];
         for(let i = 0; markerData[i]; i++) {
@@ -230,7 +238,7 @@ const Main = () => {
     }
 
 
-    // 마커 설정하기
+    // 그리드 설정하기
     const setSquares = (map) => {
         for(let i = 0; i < squares.length; i++){
             squares[i].setMap(map);
@@ -247,19 +255,6 @@ const Main = () => {
         setSquares(null);
     }
 
-    // 그리드 테스트 코드
-    const squareTest = (level) => {
-        let gridData = JSON.stringify(GridPosition);
-        gridData = JSON.parse(gridData)['data'];
-        for(let i = 1; gridData[i]; i++){
-            addSquare({
-                lat1: gridData[i]['lat1'],
-                lng1: gridData[i]['lng1'],
-                lat2: gridData[i]['lat2'],
-                lng2: gridData[i]['lng2']
-            }, 0.5)
-        }
-    }
 
     function zoomIn(){
         map.setLevel(map.getLevel() - 1);
@@ -281,10 +276,20 @@ const Main = () => {
         map = new kakao.maps.Map(container, options);
         const moveLatLon = new kakao.maps.LatLng(x, y);
         // 지도 중심을 이동 시킵니다
-        console.log(map);
         map.setCenter(moveLatLon);
+        if(isMarker){
+            markerTest();
+        }else{
+            hideMarkers();
+        }
     }
-    console.log(keyword)
+
+    if(isMarker){
+        console.log(isMarker, isGrid);
+    }else{
+        console.log("!!!");
+        hideMarkers();
+    }
     return (
         <div id="map-container">
             <SearchPanel isSearch={isSearch} setIsSearch={setIsSearch} setCenter={setCenter} setKeyword={setKeyword} searchResult={searchResult} />
@@ -293,7 +298,6 @@ const Main = () => {
                 <button className="test" onClick={markerTest}>add marker test</button>
                 <button className="test" onClick={showMarkers}>show marker test</button>
                 <button className="test" onClick={hideMarkers}>hide marker test</button>
-                <button className="test" onClick={squareTest}>add square test</button>
                 <button className="test" onClick={showSquares}>show square test</button>
                 <button className="test" onClick={hideSquares}>hide square test</button>
             </div>
@@ -301,7 +305,7 @@ const Main = () => {
                 <span className="zoomInBtn" onClick={zoomIn}><img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png" alt="확대"></img></span>
                 <span className="zoomOutBtn" onClick={zoomOut}><img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png" alt="축소"></img></span>
             </div>
-            <FilterGroup />
+            <FilterGroup showMarkers={showMarkers} hideMarkers={hideMarkers} showGrids={test} hideGrids={hideSquares}/>
             <ActionGroup />
         </div>
     );
